2bd6c96476178ac68ad6f565a3039872b338c4c8
Emil Hörnlund <emil.hornlund@me.com>
2023-07-31 17:57:33 +0200

    Added environment configuration and dependencies
    - Added new dependency @nestjs/config for handling environment variables.
    - Added Joi for validation of environment variables.
    - Configured environment variables in the app module using the newly added @nestjs/config module.
    - Used these environment variables to set up the HTTP server port.
    - Added environment file '.development.env' for development environment variables.
    - Updated yarn.lock file to reflect new dependencies.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,28 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.development.env',
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string()
+          .valid('development', 'production', 'test')
+          .default('development'),
+        HTTP_PORT: Joi.number().default(8080),
+      }),
+      validationOptions: {
+        allowUnknown: true,
+        abortEarly: true,
+      },
+      expandVariables: true,
+      isGlobal: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app.module';
 
 (async () => {
   const app = await NestFactory.create(AppModule);
-  await app.listen(8080);
+
+  const configService = app.get(ConfigService);
+  const port = configService.get('HTTP_PORT');
+  await app.listen(port);
 })();

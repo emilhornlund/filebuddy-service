1d57e3375e9bf6ed83ceea51f7e15f976e89c6ee
Emil Hörnlund <emil.hornlund@me.com>
2023-07-31 18:04:39 +0200

    Add Swagger API documentation support
    - Add @nestjs/swagger as a dependency
    - Configure Swagger in the main.ts file to set up API documentation
    - Set the global route prefix to 'api'
    - Update yarn.lock to include new dependencies

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,19 @@
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 import { AppModule } from './app.module';
 
 (async () => {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('FileBuddy Service')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api_docs', app, document);
 
   const configService = app.get(ConfigService);
   const port = configService.get('HTTP_PORT');
